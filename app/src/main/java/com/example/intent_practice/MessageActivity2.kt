5e4f0c6a4ff71faa1755d180e39e9d583342bd5d
studentDG <daegu776@gmail.com>
2021-11-01 14:02:07 +0900

    메세지를 보내는 화면/ 받는 화면 추가

---
+++ app/src/main/java/com/example/intent_practice/MessageActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.intent_practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MessageActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_message2)
+    }
+}
