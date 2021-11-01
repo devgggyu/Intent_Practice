7f91ac9641ab92691f44e1c6b9a867ce0ee1e6ce
studentDG <daegu776@gmail.com>
2021-11-01 13:47:32 +0900

    finish() 이용=> 현재화면 종료, 이전화면 복귀

--- app/src/main/java/com/example/intent_practice/OtherActivity.kt
+++ app/src/main/java/com/example/intent_practice/OtherActivity.kt
@@ -12,8 +12,10 @@ class OtherActivity : AppCompatActivity() {
 
         returnTomainBtn. setOnClickListener {
 
-            val myIntent = Intent(this, MainActivity::class.java)
-            startActivity(myIntent)
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+
+            finish()
 
         }
 
