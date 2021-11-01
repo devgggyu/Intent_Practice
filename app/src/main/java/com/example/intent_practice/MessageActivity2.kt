54eb752d4fe7aae6b075e988c3b3b21d6e4f6473
studentDG <daegu776@gmail.com>
2021-11-01 14:23:11 +0900

    전달해준 문구를 메세지 화면에서 사용

--- app/src/main/java/com/example/intent_practice/MessageActivity2.kt
+++ app/src/main/java/com/example/intent_practice/MessageActivity2.kt
@@ -2,10 +2,17 @@ package com.example.intent_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_message2.*
 
 class MessageActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message2)
+
+//        이 화면에 들어올때 첨부된 데이터를 텍스트뷰에 반영
+
+       val intentMessage = intent.getStringExtra("message")
+
+        receivedMessageTxt.text = intentMessage
     }
 }
