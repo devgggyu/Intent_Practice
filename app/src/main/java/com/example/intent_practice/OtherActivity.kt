35a69eace3d5c0cc9ca5b64b621d4944db0d1da4
studentDG <daegu776@gmail.com>
2021-11-01 13:45:50 +0900

    "B"xml에서 "A"xml으로 이동하는법 배우기
    Intent 활용

--- app/src/main/java/com/example/intent_practice/OtherActivity.kt
+++ app/src/main/java/com/example/intent_practice/OtherActivity.kt
@@ -1,11 +1,21 @@
 package com.example.intent_practice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        returnTomainBtn. setOnClickListener {
+
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
     }
 }
