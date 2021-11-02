e2e2b2c7668cc5f90f416dea133eff3edc90e919
studentDG <daegu776@gmail.com>
2021-11-02 11:14:22 +0900

    닉네임 변경을 위한 화면 이동 (startActivityForResult) 사용법 익히기

--- app/src/main/java/com/example/intent_practice/EditNickMainActivity.kt
+++ app/src/main/java/com/example/intent_practice/EditNickMainActivity.kt
@@ -2,10 +2,13 @@ package com.example.intent_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nick_main.*
 
 class EditNickMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nick_main)
+
+
     }
 }
